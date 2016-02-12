086529d1ecc709e50210bbddb5a54e40637afb94
Thomas Hopkins <thomas.john.hopkins@gmail.com>
2016-02-12 12:07:00 -0500

    Add welcome message when first visiting game.

--- js/bundle.js
+++ js/bundle.js
@@ -51,9 +51,13 @@
 	    window.Game = {};
 	  }
 	
-	  this.games = [];
+	  var $canvasEl = $(".cloud-breaker");
+	  this.ctx = $canvasEl[0].getContext("2d");
+	  this.ctx.font = "24px Montserrat";
+	  this.ctx.fillStyle = "rgb(255,255,255)";
+	  this.ctx.fillText("Welcome to Cloud Breaker!", 310, 100);
+	  this.ctx.fillText("Press 'N' to start a new game", 305, 150);
 	
-	  this.screen = $(".wrapper");
 	  // this somewhat works
 	  // var scoreboard = new Scoreboard(scoreLabel);
 	  // var gameClock = new GameClock(minutesLabel, secondsLabel);
@@ -63,7 +67,7 @@
 	
 	  $(window).on("keydown", function (e) {
 	    if (e.keyCode === 78) {
-	
+	    this.ctx.clearRect(0, 0, 900, 550);
 	    new NewGame();
 	    }
 	  }).bind(this);
--- js/main.js
+++ js/main.js
@@ -5,9 +5,13 @@ var NewGame = require('./new-game.js');
     window.Game = {};
   }
 
-  this.games = [];
+  var $canvasEl = $(".cloud-breaker");
+  this.ctx = $canvasEl[0].getContext("2d");
+  this.ctx.font = "24px Montserrat";
+  this.ctx.fillStyle = "rgb(255,255,255)";
+  this.ctx.fillText("Welcome to Cloud Breaker!", 305, 100);
+  this.ctx.fillText("Press 'N' to start a new game", 300, 150);
 
-  this.screen = $(".wrapper");
   // this somewhat works
   // var scoreboard = new Scoreboard(scoreLabel);
   // var gameClock = new GameClock(minutesLabel, secondsLabel);
@@ -17,7 +21,7 @@ var NewGame = require('./new-game.js');
 
   $(window).on("keydown", function (e) {
     if (e.keyCode === 78) {
-
+    this.ctx.clearRect(0, 0, 900, 550);
     new NewGame();
     }
   }).bind(this);
